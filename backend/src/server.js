195084c959094b16fe8f6993bb45b5b1d896a583
Gabriel Antonio <gabriel@akuko.com.br>
2019-09-20 16:16:19 -0300

    instalacao de backend

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+
+mongoose.connect('mongodb://kamino.mongodb.umbler.com:44677/luluzinha')
+
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', () => {
+  console.log('conectou');
+})
+
+const server = express()
+
+const port = process.env.PORT || 3000
+
+server.get('/', (req, res) => {
+  
+})
+
+server.listen(port, () => {
+  console.log('servidor rodando');
+})
