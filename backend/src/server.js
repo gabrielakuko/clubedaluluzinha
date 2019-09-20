e70314f602592e5dc3e51a5d7ba7bf29583d1d4e
Gabriel Antonio <gabriel@akuko.com.br>
2019-09-20 16:31:38 -0300

    testando conexao  umbler

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,20 @@
 const express = require('express')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.set('useNewUrlParser', true)
-mongoose.set('useFindAndModify', false)
-mongoose.set('useCreateIndex', true)
-mongoose.set('useUnifiedTopology', true)
+// mongoose.set('useNewUrlParser', true)
+// mongoose.set('useFindAndModify', false)
+// mongoose.set('useCreateIndex', true)
+// mongoose.set('useUnifiedTopology', true)
 
-mongoose.connect('mongodb://kamino.mongodb.umbler.com:44677/luluzinha')
+// mongoose.connect('mongodb://kamino.mongodb.umbler.com:44677/luluzinha')
 
-var db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error'))
-db.once('open', () => {
-  console.log('conectou');
-})
+// var db = mongoose.connection
+// db.on('error', console.error.bind(console, 'connection error'))
+// db.once('open', () => {
+//   console.log('conectou');
+// })
 
 const server = express()
-
 const port = process.env.PORT || 3000
 
 server.get('/', (req, res) => {
