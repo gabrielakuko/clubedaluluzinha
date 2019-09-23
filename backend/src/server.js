0b7e34e466bcbc8d0a151130e728055bd189b1dd
Gabriel Antonio <gabriel@akuko.com.br>
2019-09-23 17:55:52 -0300

    teste de upload via git para Kinghost

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require("path")
-const EmailController = require('./controllers/EmailController')
+// const EmailController = require('./controllers/EmailController')
 
 
-mongoose.set('useNewUrlParser', true)
-mongoose.set('useFindAndModify', false)
-mongoose.set('useCreateIndex', true)
-mongoose.set('useUnifiedTopology', true)
+// mongoose.set('useNewUrlParser', true)
+// mongoose.set('useFindAndModify', false)
+// mongoose.set('useCreateIndex', true)
+// mongoose.set('useUnifiedTopology', true)
 
-mongoose.connect('mongodb://kamino.mongodb.umbler.com:44677/luluzinha')
+// mongoose.connect('mongodb://kamino.mongodb.umbler.com:44677/luluzinha')
 
-var db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error'))
-db.once('open', () => {
-  console.log('conectou');
-})
+// var db = mongoose.connection
+// db.on('error', console.error.bind(console, 'connection error'))
+// db.once('open', () => {
+//   console.log('conectou');
+// })
 
 const server = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 21096
 
 server.use(express.static(__dirname))
 server.use(express.static(path.join(__dirname, 'frontend/build')))
