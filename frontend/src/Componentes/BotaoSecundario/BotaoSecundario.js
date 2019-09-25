2dc5d7f081cfa24e53d6a09539ec4c6a2fe86a38
Gabriel Felipe <Gabriel_fcs@outlook.com>
2019-09-25 02:59:24 -0300

    fix(secondary-button): add estructure to support url formats

--- frontend/src/Componentes/BotaoSecundario/BotaoSecundario.js
+++ frontend/src/Componentes/BotaoSecundario/BotaoSecundario.js
@@ -5,12 +5,18 @@ import { Link } from 'react-router-dom'; // Importando react router dom
 import './BotaoSecundario.css'
 
 const BotaoSecundario = props => {
-  if (props.linkBotaoSecundario) {
-    return (
-      <Link to={props.linkBotaoSecundario} className="botaoSecundario" onClick={props.cliqueFuncao}>{props.textoBotao}</Link>
-    );
+  if (!props.linkBotaoSecundario) {
+    return '';
   }
-  return '';
+  if (typeof props.linkBotaoSecundario === 'string') {
+    return (
+      <a href={props.linkBotaoSecundario} className="botaoSecundario" onClick={props.cliqueFuncao}>{props.textoBotao}</a>
+    );  
+  }
+
+  return (
+    <Link to={props.linkBotaoSecundario} className="botaoSecundario" onClick={props.cliqueFuncao}>{props.textoBotao}</Link>
+  );
 }
 
 export default BotaoSecundario
