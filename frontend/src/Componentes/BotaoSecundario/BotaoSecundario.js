ea75aa815ce06167d91e2db5cc8326e5d06b29c3
Gabriel Felipe <Gabriel_fcs@outlook.com>
2019-09-25 03:09:51 -0300

    fix(secondary-button): allow buttons without url

--- frontend/src/Componentes/BotaoSecundario/BotaoSecundario.js
+++ frontend/src/Componentes/BotaoSecundario/BotaoSecundario.js
@@ -5,10 +5,7 @@ import { Link } from 'react-router-dom'; // Importando react router dom
 import './BotaoSecundario.css'
 
 const BotaoSecundario = props => {
-  if (!props.linkBotaoSecundario) {
-    return '';
-  }
-  if (typeof props.linkBotaoSecundario === 'string') {
+  if (!props.linkBotaoSecundario || typeof props.linkBotaoSecundario === 'string') {
     return (
       <a href={props.linkBotaoSecundario} className="botaoSecundario" onClick={props.cliqueFuncao}>{props.textoBotao}</a>
     );  
