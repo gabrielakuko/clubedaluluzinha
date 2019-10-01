6aae5759fd9d1e57ff13ba4670c49bd678901ab3
Gabriel Antonio <gabriel@akuko.com.br>
2019-10-01 16:26:43 -0300

    Fix(Equipe, SecaoTime): Fixing the text of the team area

--- frontend/src/Componentes/Equipe/Equipe.js
+++ frontend/src/Componentes/Equipe/Equipe.js
@@ -6,7 +6,7 @@ const Equipe = props => (
   <div className="equipe">
     <div className="containerImagemEquipe"><img src={props.imagemEquipe} className="imagemEquipe" alt="Imagem da embaixadora Greice"/></div>
     <h4>{props.nomeEquipe}</h4>
-    <p>{props.descricaoEquipe}</p>
+    <p>{props.children}</p>
   </div>
 )
 
--- frontend/src/Componentes/SecaoTime/SecaoTime.js
+++ frontend/src/Componentes/SecaoTime/SecaoTime.js
@@ -12,10 +12,27 @@ import './SecaoTime.css'
 const SecaoTime = props => (
   <div className="secaoTime container">
 
-    <Equipe nomeEquipe="Daiane Steffen" imagemEquipe={Daiane} descricaoEquipe="Administradora, especializada em Marketing e Gestão Comercial. Com mais de 15 anos de experiência em Planejamento Estratégico, Marketing e Vendas."/>
+    <Equipe nomeEquipe="Daiane Steffen" imagemEquipe={Daiane} >
+      <ul className="lista-equipe">
+        <li>+ de 15 anos de experiência em planejamento estratégico, marketing e vendas</li>
+        <li>Pós Graduação em Marketing pela Master of Business Administration - Universidade de Salamanca na Espanha</li>
+        <li>Planejamento e Gestão Comercial com foco em influência na estratégia de gestão</li>
+        <li>Integração de processos CRM</li>
+        <li>Estruturação de equipe e inteligência mercadológica</li>
+        <li>Gestão de experiência do cliente e estratégia comercial</li>
+      </ul>
+    </Equipe>
 
-    <Equipe nomeEquipe="Paula Steffen" imagemEquipe={Paula} descricaoEquipe="Empresária, publicitária, é focada em resultados e gosta de fazer isso com leveza e sorriso aberto.
-Com mais de 15 anos de experiência em gestão comercial e gerenciamento de competências."/>
+    <Equipe nomeEquipe="Paula Steffen" imagemEquipe={Paula}>
+      <ul className="lista-equipe">
+        <li>+ de 15 anos de experiência em Gestão Comercial e Gerenciamento de Competências</li>
+        <li>Especialização em Marketing para Gestão Empresarial - UFSC</li>
+        <li>MBA em Gestão de Pessoas pela Universidade Candido Mendes</li>
+        <li>Gestão de projetos comerciais e formação de equipes engajadas</li>
+        <li>Coordenação de comunicação e imagem da marca</li>
+        <li className="sem-list">"Já fui aquela que dava o sangue pela empresa e trazia resultados para um só. Agora faço isso para impulsionar vários."</li>
+      </ul>
+    </Equipe>
     
   </div>
 )
