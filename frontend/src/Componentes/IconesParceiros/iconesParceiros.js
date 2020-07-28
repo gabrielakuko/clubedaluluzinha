5166e3e993d8c8829e16bc570beda3106c46cf96
Gabriel Antonio <gabriel@akuko.com.br>
2020-07-27 21:27:27 -0300

    Novo componente de icones de parceiros
    Ainda faltam alguns ícones

--- frontend/src/Componentes/IconesParceiros/iconesParceiros.js
+++ frontend/src/Componentes/IconesParceiros/iconesParceiros.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'; // Importando react router dom
+
 
 import './iconesParceiros.css'
 
 const iconesParceiros = props => (
   <div className="iconesParceiros">
-    <a href={props.linkParceiro}>
+    <Link to="/parceiros">
       <img src={props.imagemParceiro} alt={props.altParceiro}/>
-    </a>
+    </Link>
   </div>
 )
 
